c8e0954386268826a7635537bf8a37b85db67c40
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-04-06 15:43:56 -0600

    make http request and get back serach results data

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,7 +1,45 @@
 /**
  * Created by rachelkoldenhoven on 4/6/16.
  */
-app.controller('MovieController', ['$scope', function($scope) {
+app.controller('MovieController', ['$scope', "MovieSearchService", function($scope, MovieSearchService) {
+  $scope.view = {};
   $scope.greeting = "Hello World!";
   $scope.message = "Views are working!";
+
+
+  $scope.movieSearch = function(title) {
+    console.log(title);
+    MovieSearchService.movieSearchResults(title).then(function(data) {
+      $scope.view.searchResults = data.data.Search;
+      console.log($scope.view.searchResults);
+    })
+  }
+
 }]);
+
+
+
+//app.controller('PokemonController', ["$scope", "PokemonService", function($scope, PokemonService) {
+//  $scope.view = {};
+//  $scope.view.display = "";
+//  $scope.newPokemon = {};
+//  $scope.newMove = {};
+//
+//  $scope.getNewPokemon = function() {
+//    $scope.loading = true;
+//    PokemonService.generatePokemon().then(function(data){
+//      $scope.newPokemon = data.data;
+//      $scope.loading = false;
+//      console.log($scope.newPokemon);
+//    });
+//  };
+//
+//  $scope.getNewMove = function() {
+//    PokemonService.generateMove().then(function(data){
+//      $scope.newMove = data.data;
+//      console.log($scope.newMove.name);
+//    });
+//  };
+//
+//
+//}]);
--- src/js/services.js
+++ src/js/services.js
@@ -1,3 +1,13 @@
 /**
  * Created by rachelkoldenhoven on 4/6/16.
  */
+app.service('MovieSearchService', ['$http', function ($http) {
+  return {
+    movieSearchResults: function (title) {
+      return $http.get('http://www.omdbapi.com/?s=' + title);
+    },
+    getMovieDetails: function() {
+      return $http.get('http://www.omdbapi.com/?');
+    }
+  }
+}]);
